8bbb9dd74fdfdd747fb54675dba9eada44a8852a
Elijah Ahianyo <elijahahianyo@gmail.com>
2021-03-15 22:58:07 +0000

    refactored package naming structure as well as fixed application start failure issue

--- src/test/java/com/TradeProject/ClientConnectivity/ClientConnectivityApplicationTests.java
+++ src/test/java/com/TradeProject/ClientConnectivity/ClientConnectivityApplicationTests.java
@@ -1,4 +1,4 @@
-package io.turntabl.clientConnectivity;
+package com.TradeProject.ClientConnectivity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
